544dc86ac7e3706c069e083d1af39132bddbd9f2
Nav Volcy <navvolcy@gmail.com>
2025-07-19 00:44:52 -0600

    Add project files.

---
+++ StudentCellPhone/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentCellPhone.Controllers
+{
+    public class StudentController : Controller
+    {
+        //Get: /Student 
+        public string Index()
+        {
+            return "this is my default action";
+        }
+        //
+        // GET: / Student/Welcome/
+        public string WelcomeStudent()
+        {
+            return " this is the Welcome Student action method...";
+        }
+    }
+        
+}
